c84309fbc994ed125006baf21c4cb305c7f1b918
강민규 <@DESKTOP-7A2RC6P>
2026-03-26 12:12:54 +0900

    과제4 코딩과 테스트 완료 나누기 0 발생 시 무한대(∞) 기호 감지 및 결과창 안내 문구 출력 로직 강화 연산자 중복 입력 방지(자동 교체), 소수점(.), 부호 전환(+/-) 및 Null 안정성 확보

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -49,13 +49,15 @@
             btnCE = new Button();
             btnClear = new Button();
             btnDelete = new Button();
+            btnDot = new Button();
+            btnSign = new Button();
             SuspendLayout();
             // 
             // btn7
             // 
-            btn7.Location = new Point(206, 189);
+            btn7.Location = new Point(56, 249);
             btn7.Name = "btn7";
-            btn7.Size = new Size(112, 50);
+            btn7.Size = new Size(112, 75);
             btn7.TabIndex = 0;
             btn7.Text = "7";
             btn7.UseVisualStyleBackColor = true;
@@ -63,9 +65,9 @@
             // 
             // btn8
             // 
-            btn8.Location = new Point(324, 189);
+            btn8.Location = new Point(174, 249);
             btn8.Name = "btn8";
-            btn8.Size = new Size(112, 50);
+            btn8.Size = new Size(112, 75);
             btn8.TabIndex = 1;
             btn8.Text = "8";
             btn8.UseVisualStyleBackColor = true;
@@ -73,9 +75,9 @@
             // 
             // btn9
             // 
-            btn9.Location = new Point(442, 189);
+            btn9.Location = new Point(292, 249);
             btn9.Name = "btn9";
-            btn9.Size = new Size(112, 50);
+            btn9.Size = new Size(112, 75);
             btn9.TabIndex = 2;
             btn9.Text = "9";
             btn9.UseVisualStyleBackColor = true;
@@ -83,9 +85,9 @@
             // 
             // btn4
             // 
-            btn4.Location = new Point(206, 245);
+            btn4.Location = new Point(56, 330);
             btn4.Name = "btn4";
-            btn4.Size = new Size(112, 50);
+            btn4.Size = new Size(112, 75);
             btn4.TabIndex = 3;
             btn4.Text = "4";
             btn4.UseVisualStyleBackColor = true;
@@ -93,9 +95,9 @@
             // 
             // btn5
             // 
-            btn5.Location = new Point(324, 245);
+            btn5.Location = new Point(174, 330);
             btn5.Name = "btn5";
-            btn5.Size = new Size(112, 50);
+            btn5.Size = new Size(112, 75);
             btn5.TabIndex = 4;
             btn5.Text = "5";
             btn5.UseVisualStyleBackColor = true;
@@ -103,9 +105,9 @@
             // 
             // btn6
             // 
-            btn6.Location = new Point(442, 245);
+            btn6.Location = new Point(292, 330);
             btn6.Name = "btn6";
-            btn6.Size = new Size(112, 50);
+            btn6.Size = new Size(112, 75);
             btn6.TabIndex = 5;
             btn6.Text = "6";
             btn6.UseVisualStyleBackColor = true;
@@ -113,9 +115,9 @@
             // 
             // btn1
             // 
-            btn1.Location = new Point(206, 301);
+            btn1.Location = new Point(56, 411);
             btn1.Name = "btn1";
-            btn1.Size = new Size(112, 50);
+            btn1.Size = new Size(112, 75);
             btn1.TabIndex = 6;
             btn1.Text = "1";
             btn1.UseVisualStyleBackColor = true;
@@ -123,9 +125,9 @@
             // 
             // btn2
             // 
-            btn2.Location = new Point(324, 301);
+            btn2.Location = new Point(174, 411);
             btn2.Name = "btn2";
-            btn2.Size = new Size(112, 50);
+            btn2.Size = new Size(112, 75);
             btn2.TabIndex = 7;
             btn2.Text = "2";
             btn2.UseVisualStyleBackColor = true;
@@ -133,9 +135,9 @@
             // 
             // btn3
             // 
-            btn3.Location = new Point(442, 301);
+            btn3.Location = new Point(292, 411);
             btn3.Name = "btn3";
-            btn3.Size = new Size(112, 50);
+            btn3.Size = new Size(112, 75);
             btn3.TabIndex = 8;
             btn3.Text = "3";
             btn3.UseVisualStyleBackColor = true;
@@ -143,9 +145,9 @@
             // 
             // btn0
             // 
-            btn0.Location = new Point(324, 357);
+            btn0.Location = new Point(174, 492);
             btn0.Name = "btn0";
-            btn0.Size = new Size(112, 50);
+            btn0.Size = new Size(112, 75);
             btn0.TabIndex = 9;
             btn0.Text = "0";
             btn0.UseVisualStyleBackColor = true;
@@ -153,115 +155,149 @@
             // 
             // txtResult
             // 
-            txtResult.Location = new Point(206, 101);
+            txtResult.BackColor = SystemColors.HighlightText;
+            txtResult.Font = new Font("맑은 고딕", 9F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            txtResult.Location = new Point(56, 121);
             txtResult.Name = "txtResult";
             txtResult.Size = new Size(466, 31);
             txtResult.TabIndex = 10;
             // 
             // txtExpression
             // 
-            txtExpression.Location = new Point(206, 64);
+            txtExpression.BackColor = SystemColors.HighlightText;
+            txtExpression.Font = new Font("맑은 고딕", 9F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            txtExpression.Location = new Point(56, 75);
             txtExpression.Name = "txtExpression";
             txtExpression.Size = new Size(466, 31);
             txtExpression.TabIndex = 11;
             // 
             // btnEqual
             // 
-            btnEqual.Location = new Point(560, 357);
+            btnEqual.BackColor = Color.FromArgb(255, 224, 192);
+            btnEqual.Location = new Point(410, 492);
             btnEqual.Name = "btnEqual";
-            btnEqual.Size = new Size(112, 50);
+            btnEqual.Size = new Size(112, 75);
             btnEqual.TabIndex = 12;
             btnEqual.Text = "=";
-            btnEqual.UseVisualStyleBackColor = true;
+            btnEqual.UseVisualStyleBackColor = false;
             btnEqual.Click += btnEqual_Click;
             // 
             // btnPlus
             // 
-            btnPlus.Location = new Point(560, 301);
+            btnPlus.BackColor = Color.FromArgb(255, 224, 192);
+            btnPlus.Location = new Point(410, 411);
             btnPlus.Name = "btnPlus";
-            btnPlus.Size = new Size(112, 50);
+            btnPlus.Size = new Size(112, 75);
             btnPlus.TabIndex = 13;
             btnPlus.Text = "+";
-            btnPlus.UseVisualStyleBackColor = true;
+            btnPlus.UseVisualStyleBackColor = false;
             btnPlus.Click += btnOperator_Click;
             // 
             // lblTitle
             // 
             lblTitle.AutoSize = true;
-            lblTitle.Font = new Font("Tahoma", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTitle.Font = new Font("Tahoma", 22F, FontStyle.Bold, GraphicsUnit.Point, 0);
             lblTitle.ForeColor = SystemColors.ActiveCaption;
-            lblTitle.Location = new Point(206, 9);
+            lblTitle.Location = new Point(94, 9);
             lblTitle.Name = "lblTitle";
-            lblTitle.Size = new Size(338, 43);
+            lblTitle.Size = new Size(408, 53);
             lblTitle.TabIndex = 14;
             lblTitle.Text = "Simple Calculator\r\n";
             // 
             // btnMulti
             // 
-            btnMulti.Location = new Point(560, 189);
+            btnMulti.BackColor = Color.FromArgb(255, 224, 192);
+            btnMulti.Location = new Point(410, 249);
             btnMulti.Name = "btnMulti";
-            btnMulti.Size = new Size(112, 50);
+            btnMulti.Size = new Size(112, 75);
             btnMulti.TabIndex = 15;
             btnMulti.Text = "×";
-            btnMulti.UseVisualStyleBackColor = true;
+            btnMulti.UseVisualStyleBackColor = false;
             btnMulti.Click += btnOperator_Click;
             // 
             // btnDiv
             // 
-            btnDiv.Location = new Point(560, 133);
+            btnDiv.BackColor = Color.FromArgb(255, 224, 192);
+            btnDiv.Location = new Point(410, 168);
             btnDiv.Name = "btnDiv";
-            btnDiv.Size = new Size(112, 50);
+            btnDiv.Size = new Size(112, 75);
             btnDiv.TabIndex = 16;
             btnDiv.Text = "÷";
-            btnDiv.UseVisualStyleBackColor = true;
+            btnDiv.UseVisualStyleBackColor = false;
             btnDiv.Click += btnOperator_Click;
             // 
             // btnMinus
             // 
-            btnMinus.Location = new Point(560, 245);
+            btnMinus.BackColor = Color.FromArgb(255, 224, 192);
+            btnMinus.Location = new Point(410, 330);
             btnMinus.Name = "btnMinus";
-            btnMinus.Size = new Size(112, 50);
+            btnMinus.Size = new Size(112, 75);
             btnMinus.TabIndex = 17;
             btnMinus.Text = "-";
-            btnMinus.UseVisualStyleBackColor = true;
+            btnMinus.UseVisualStyleBackColor = false;
             btnMinus.Click += btnOperator_Click;
             // 
             // btnCE
             // 
-            btnCE.Location = new Point(206, 133);
+            btnCE.BackColor = Color.FromArgb(255, 128, 128);
+            btnCE.Location = new Point(56, 168);
             btnCE.Name = "btnCE";
-            btnCE.Size = new Size(112, 50);
+            btnCE.Size = new Size(112, 75);
             btnCE.TabIndex = 18;
             btnCE.Text = "CE";
-            btnCE.UseVisualStyleBackColor = true;
+            btnCE.UseVisualStyleBackColor = false;
             btnCE.Click += btnCE_Click;
             // 
             // btnClear
             // 
-            btnClear.Location = new Point(324, 133);
+            btnClear.BackColor = Color.FromArgb(255, 128, 128);
+            btnClear.Location = new Point(174, 168);
             btnClear.Name = "btnClear";
-            btnClear.Size = new Size(112, 50);
+            btnClear.Size = new Size(112, 75);
             btnClear.TabIndex = 19;
             btnClear.Text = "C";
-            btnClear.UseVisualStyleBackColor = true;
+            btnClear.UseVisualStyleBackColor = false;
             btnClear.Click += btnClear_Click;
             // 
             // btnDelete
             // 
-            btnDelete.Location = new Point(442, 133);
+            btnDelete.BackColor = Color.FromArgb(255, 128, 128);
+            btnDelete.Location = new Point(292, 168);
             btnDelete.Name = "btnDelete";
-            btnDelete.Size = new Size(112, 50);
+            btnDelete.Size = new Size(112, 75);
             btnDelete.TabIndex = 20;
             btnDelete.Text = "del";
-            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.UseVisualStyleBackColor = false;
             btnDelete.Click += btnDelete_Click;
+            // 
+            // btnDot
+            // 
+            btnDot.Location = new Point(292, 492);
+            btnDot.Name = "btnDot";
+            btnDot.Size = new Size(112, 75);
+            btnDot.TabIndex = 21;
+            btnDot.Text = ".";
+            btnDot.UseVisualStyleBackColor = true;
+            btnDot.Click += btnDot_Click;
+            // 
+            // btnSign
+            // 
+            btnSign.Location = new Point(56, 492);
+            btnSign.Name = "btnSign";
+            btnSign.Size = new Size(112, 75);
+            btnSign.TabIndex = 22;
+            btnSign.Text = "+/-";
+            btnSign.UseVisualStyleBackColor = true;
+            btnSign.Click += btnSign_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = SystemColors.ButtonFace;
-            ClientSize = new Size(800, 450);
+            BackColor = Color.FromArgb(255, 192, 192);
+            ClientSize = new Size(574, 627);
+            Controls.Add(btnSign);
+            Controls.Add(btnDot);
             Controls.Add(btnDelete);
             Controls.Add(btnClear);
             Controls.Add(btnCE);
@@ -314,5 +350,7 @@
         private Button btnCE;
         private Button btnClear;
         private Button btnDelete;
+        private Button btnDot;
+        private Button btnSign;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -16,79 +16,112 @@ namespace SimpleCalculator
 
         }
 
-        // [과제 1 & 2] 숫자 버튼 클릭
+        // 1. 숫자 버튼 클릭
         private void btnNumber_Click(object sender, EventArgs e)
         {
-            // 계산이 끝난 후(=이 포함된 상태) 숫자를 누르면 새로 시작하도록 초기화
             if (txtExpression.Text.Contains("=")) txtExpression.Clear();
-
-            Button btn = (Button)sender;
-            txtExpression.Text += btn.Text;
+            if (sender is Button btn)
+            {
+                if (txtExpression.Text == "0" || txtExpression.Text.EndsWith(" 0"))
+                {
+                    txtExpression.Text = txtExpression.Text.Substring(0, txtExpression.Text.Length - 1);
+                }
+                txtExpression.Text += btn.Text;
+            }
         }
 
-        // [과제 2] 연산자 버튼 클릭
+        // 2. 연산자 버튼 (중복 시 교체 로직 포함)
         private void btnOperator_Click(object sender, EventArgs e)
         {
-            // 계산 결과 뒤에 바로 연산자를 붙여서 이어서 계산 가능하게 처리
-            if (txtExpression.Text.Contains("="))
-            {
-                txtExpression.Text = txtResult.Text;
-            }
+            if (txtExpression.Text.Contains("=")) txtExpression.Text = txtResult.Text;
+            if (string.IsNullOrWhiteSpace(txtExpression.Text)) return;
 
-            Button btn = (Button)sender;
-            txtExpression.Text += " " + btn.Text + " ";
+            if (sender is Button btn)
+            {
+                if (txtExpression.Text.EndsWith(" "))
+                {
+                    txtExpression.Text = txtExpression.Text.Substring(0, txtExpression.Text.Length - 3);
+                }
+                txtExpression.Text += " " + btn.Text + " ";
+            }
         }
 
-        // [과제 3] CE: 마지막 숫자 뭉치 삭제
+        // 3. 소수점, 부호 전환, 삭제 등은 기존과 동일
+        private void btnDot_Click(object sender, EventArgs e)
+        {
+            if (txtExpression.Text.Contains("=")) txtExpression.Clear();
+            string[] parts = txtExpression.Text.Split(' ');
+            string lastPart = parts[parts.Length - 1];
+            if (!lastPart.Contains("."))
+            {
+                if (string.IsNullOrEmpty(lastPart)) txtExpression.Text += "0.";
+                else txtExpression.Text += ".";
+            }
+        }
+
+        private void btnSign_Click(object sender, EventArgs e)
+        {
+            if (txtExpression.Text.Contains("=")) return;
+            string text = txtExpression.Text.TrimEnd();
+            int lastSpaceIndex = text.LastIndexOf(' ');
+            string prefix = lastSpaceIndex == -1 ? "" : text.Substring(0, lastSpaceIndex + 1);
+            string lastNumber = lastSpaceIndex == -1 ? text : text.Substring(lastSpaceIndex + 1);
+            if (string.IsNullOrEmpty(lastNumber) || lastNumber == "0") return;
+
+            if (lastNumber.StartsWith("-")) txtExpression.Text = prefix + lastNumber.Substring(1);
+            else txtExpression.Text = prefix + "-" + lastNumber;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e) { txtExpression.Clear(); txtResult.Clear(); }
+
         private void btnCE_Click(object sender, EventArgs e)
         {
-            if (txtExpression.Text.Contains("=")) { btnClear_Click(null, null); return; }
-
-            string text = txtExpression.Text.TrimEnd();
-            if (string.IsNullOrEmpty(text)) return;
-            int lastSpaceIndex = text.LastIndexOf(' ');
-            if (lastSpaceIndex == -1) txtExpression.Clear();
-            else txtExpression.Text = text.Substring(0, lastSpaceIndex + 1);
-        }
-
-        // [과제 3] C: 초기화
-        private void btnClear_Click(object sender, EventArgs e)
-        {
-            txtExpression.Clear();
+            if (txtExpression.Text.Contains("=")) { btnClear_Click(sender, e); return; }
+            string text = txtExpression.Text;
+            int lastSpaceIndex = text.TrimEnd().LastIndexOf(' ');
+            if (lastSpaceIndex == -1) txtExpression.Text = "0";
+            else txtExpression.Text = text.Substring(0, lastSpaceIndex + 1) + "0";
             txtResult.Clear();
         }
 
-        // [과제 3] Del: 한 글자 삭제
         private void btnDelete_Click(object sender, EventArgs e)
         {
-            if (txtExpression.Text.Contains("=")) return; // 결과 도출 후엔 삭제 불가
-            if (txtExpression.Text.Length > 0)
-                txtExpression.Text = txtExpression.Text.Substring(0, txtExpression.Text.Length - 1);
+            if (txtExpression.Text.Contains("=") || txtExpression.Text.Length == 0) return;
+            txtExpression.Text = txtExpression.Text.Substring(0, txtExpression.Text.Length - 1);
         }
 
-        // ★ 수정된 부분: 결과창과 수식창에 동시에 값 표시 ★
+        // ★ [핵심 수정] 결과 계산 및 무한대(∞) 처리 ★
         private void btnEqual_Click(object sender, EventArgs e)
         {
             if (string.IsNullOrWhiteSpace(txtExpression.Text) || txtExpression.Text.Contains("=")) return;
+            if (txtExpression.Text.EndsWith(" ")) return;
 
             try
             {
-                string originalExpression = txtExpression.Text; // 원래 수식 보관
-                string solveExpression = originalExpression.Replace("×", "*").Replace("÷", "/");
+                string original = txtExpression.Text;
+                string solve = original.Replace("×", "*").Replace("÷", "/");
 
                 DataTable table = new DataTable();
-                var calcResult = table.Compute(solveExpression, "");
-                string resultStr = calcResult.ToString();
+                object? calcResult = table.Compute(solve, "");
 
-                // 1. 하단 결과창에는 숫자만 표시
-                txtResult.Text = resultStr;
+                if (calcResult == null) return;
 
-                // 2. 상단 수식창에는 "기존 수식 = 결과" 형태로 표시
-                txtExpression.Text = originalExpression + " = " + resultStr;
+                string resultStr = calcResult.ToString() ?? "";
+
+                // ∞ 기호나 Infinity 텍스트가 포함되어 있는지 검사
+                if (resultStr.Contains("∞") || resultStr.ToLower().Contains("infinity") || resultStr.Contains("nan"))
+                {
+                    txtResult.Text = "0으로 나눌 수 없습니다.";
+                }
+                else
+                {
+                    txtResult.Text = resultStr;
+                    txtExpression.Text = original + " = " + resultStr;
+                }
             }
             catch (Exception)
             {
-                MessageBox.Show("수식을 확인해주세요.");
+                txtResult.Text = "수식 오류";
             }
         }
     }
